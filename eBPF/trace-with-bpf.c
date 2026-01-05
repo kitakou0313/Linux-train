2047e7c4810af65eb67b88c898a00a53b4b282e5
kitakou0313 <kitakou0313@gmail.com>
2026-01-06 08:08:53 +0900

    add: add ebpf sample code

--- eBPF/trace-with-bpf.c
+++ eBPF/trace-with-bpf.c
@@ -7,15 +7,24 @@ struct event
 {
     /* data */
     __u32 pid;
-    __u32 ppid;
+    __u32 tid;
     char comm[16];
     char filename[256];
 };
 
 // リングバッファを定義
 struct {
-    __uint(type, BPF_MAP_TYPE_RINGBUF)
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024);
 } events SEC(".maps");
+
+// sys_enter_execvに対応したTracepointのコンテキストを表す構造体
+struct trace_event_raw_sys_enter
+{
+    __u64 unused;
+    long syscall_nr;
+    unsigned long args[6];
+};
 
 // SECマクロによりBPFプログラムを実行するentry pointを指定
 // HTMLのイベントリスナの登録と同じ感じ？
@@ -23,7 +32,7 @@ SEC("tp/syscalls/sys_enter_execve")
 int trace_execve(struct trace_event_raw_sys_enter *ctx)
 {
     struct  event *e;
-    struct task_struct *task;
+    __u64 pid_tgid;
 
     // ring bufferの領域を確保
     e = bpf_ringbuf_reserve(
@@ -36,13 +45,12 @@ int trace_execve(struct trace_event_raw_sys_enter *ctx)
         /* code */
         return 0;
     }
-    
-    // 現在のタスクの構造体を取得
-    task = (struct task_struct *)bpf_get_current_task();
 
-    // データを構造体に追加
-    e->pid = bpf_get_current_pid_tgid() >> 32;
-    e->ppid = BPF_CORE_READ(task, real_parent, tgid);
+    // データを取得し、保存
+    pid_tgid = bpf_get_current_pid_tgid();
+    e->pid = pid_tgid >> 32;
+    e->tid = pid_tgid;
+    
     bpf_get_current_comm(&e->comm, sizeof(e->comm));
 
     // 実行ファイル名を取得
